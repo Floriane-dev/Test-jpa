6c42b8cfa48c4d160d4519d6c80e603481c49972
Floriane-dev <91901779+Floriane-dev@users.noreply.github.com>
2021-10-16 08:47:50 +0200

    TP3

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -38,39 +38,43 @@ public class App
          /**J'ajoute le nouveau livre dans la BD :
          // em.persist(l2);
 
+          */
          // Je modifie le titre du livre l2 :
-           l2 = em.find(Livre.class,9);
-           l2.setTitre("Je suis le nouv titre de l2");
+           l2 = em.find(Livre.class,5);
+           l2.setTitre("Du plaisir dans la cuisine");
            em.merge(l2);  //UPDATE pour la modif du titre
 
+         /**
          //Je supprime l2 :
            em.remove(l2);
 
-          */
-          //Requête pour extraire un livre en fonction de son titre :
-         TypedQuery<Livre> requeteTitre = em.createQuery("select i from Livre i where i.titre='Je suis le nouv titre de l2'", Livre.class);
-         Livre sortie = requeteTitre.getSingleResult();
-         System.out.println("Le livre dont le titre est 'Du plaisir dans la cuisine' a pour id "+ sortie.getId());
-
-         //Requête pour extraire  livre à partir du nom de l'auteur :
-         TypedQuery<Livre> requeteAuteur = em.createQuery("Select i from Livre i where i.auteur = 'Gaston Pouet'", Livre.class);
-         Livre sortieBis = requeteAuteur.getSingleResult();
-         System.out.println("L'identifiant du livre de Gaston Pouet est " + sortieBis.getId());
-
-         //Je liste tous les elements de la BD :
-         TypedQuery<Livre> requeteTous = em.createQuery("Select i from Livre i", Livre.class);
-         List<Livre> tous = null;
-         tous = requeteTous.getResultList();
-         for(Livre tt : tous) {
-             System.out.println("Auteur : " + tt.getAuteur() + " Titre : " + tt.getTitre());
-
-         }
           /**
           * Valide la Transaction et la ferme la transaction
           */
          em.getTransaction().commit();
-         em.close();
 
-    }
+
+         //Les requêtes JPQL n'ont pas besoin d'être encadré par une trasaction begin/commit
+    //Requête pour extraire un livre en fonction de son titre :
+    TypedQuery<Livre> requeteTitre = em.createQuery("select i from Livre i where i.titre='Germinal'", Livre.class);
+    Livre sortie = requeteTitre.getSingleResult();
+         System.out.println("Le livre dont le titre est 'Germinal' "+ sortie.getId());
+
+
+    //Requête pour extraire  livre à partir du nom de l'auteur :
+    TypedQuery<Livre> requeteAuteur = em.createQuery("Select i from Livre i where i.auteur = 'Gaston Pouet'", Livre.class);
+    Livre sortieBis = requeteAuteur.getSingleResult();
+         System.out.println("L'identifiant du livre de Gaston Pouet est " + sortieBis.getId());
+
+    //Je liste tous les elements de la BD :
+    TypedQuery<Livre> requeteTous = em.createQuery("Select i from Livre i", Livre.class);
+    List<Livre> tous = null;
+    tous = requeteTous.getResultList();
+         for(Livre tt : tous) {
+             System.out.println("Auteur : " + tt.getAuteur() + " Titre : " + tt.getTitre());
+         }
+
+         em.close();
+}
 
 }
