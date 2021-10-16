0f62064cd9d1c8a29c0005fe0171e88b619e097a
Floriane-dev <91901779+Floriane-dev@users.noreply.github.com>
2021-10-16 08:48:38 +0200

    TP3

---
+++ src/main/java/org/example/modele/Client.java
@@ -0,0 +1,47 @@
+package org.example.modele;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "CLIENT")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idClient;
+
+    @Column(name = "NOM", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "PRENOM", length = 50, nullable = false)
+    private String prenom;
+
+    @OneToMany(mappedBy = "clientE")
+    private Set<Emprunt> emprunts;
+
+    //Constructeur par défaut obligatoire
+    public Client(){
+        super();
+
+        //rajout l'instanciation de l'emprunt
+        emprunts = new HashSet<Emprunt>();
+    }
+    public Client(int idClient, String name, String prenom){
+     this.idClient = idClient;
+     this.name = name;
+     this.prenom = prenom;
+    }
+    //Getter et setter
+    public int getIdClient(){ return idClient; }
+    public void setIdClient (int idClient){ this.idClient = idClient; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getPrenom() { return prenom; }
+    public void setPrenom(String prenom) { this.prenom = prenom; }
+
+    public Set<Emprunt> getEmprunts() { return emprunts; }
+    public void setEmprunts(Set<Emprunt> emprunts) { this.emprunts = emprunts; }
+}
---
+++ src/main/java/org/example/modele/Emprunt.java
@@ -0,0 +1,48 @@
+package org.example.modele;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "EMPRUNT")
+public class Emprunt {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idEmprunt;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "DATE_DEBUT", nullable = false)
+    private Date dateDeb;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "DATE_FIN", nullable = true)
+    private Date dateFin;
+
+    @Column(name ="DELAI", length =10, nullable = true)
+    private int delaiMax;
+
+    @ManyToOne
+    @JoinColumn(name = "ID_CLIENT", nullable = false) //@JoinColumn : la colonne client pointe vers la classe emprunt
+    private Client clientE; //un lien : clé etrangere vers l'entite client
+
+    //constructeur par défaut
+    public Emprunt() {
+    }
+
+    //getter et setter
+    public int getIdEmprunt() { return idEmprunt; }
+    public void setIdEmprunt(int idEmprunt) { this.idEmprunt = idEmprunt; }
+
+    public Date getDateDeb() { return dateDeb; }
+    public void setDateDeb(Date dateDeb) { this.dateDeb = dateDeb; }
+
+    public Date getDateFin() { return dateFin; }
+    public void setDateFin(Date dateFin) { this.dateFin = dateFin; }
+
+    public int getDelaiMax() { return delaiMax; }
+    public void setDelaiMax(int delaiMax) { this.delaiMax = delaiMax; }
+
+    public Client getClientE() { return clientE; }
+    public void setClientE(Client clientE) { this.clientE = clientE; }
+
+}
